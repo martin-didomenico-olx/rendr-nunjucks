c1a429d68ecc864ab68eaaf76249144074ce9adc
Martin Di Domenico <martin.didomenico@olx.com>
2014-04-28 16:16:07 -0300

    Require compiled
    Fixed routes

--- shared/templateFinder.js
+++ shared/templateFinder.js
@@ -4,7 +4,7 @@ module.exports = function(nunjucks) {
     return {
         getTemplate: function(name) {
             return function(data) {
-                return nunjucks.render(name + '.html', data);
+                return nunjucks.render('compiled/' + name + '.html', data);
             };
         }
     };
