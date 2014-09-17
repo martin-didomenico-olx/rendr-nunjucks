bd1683674d5f1044738e50d06b2c770d6cf3aafd
Martin Di Domenico <martin.didomenico@olx.com>
2014-09-17 10:32:42 -0300

    CPU
    Improvements

--- index.js
+++ index.js
@@ -1,50 +1,46 @@
 'use strict';
 
-var nunjucks;
+var nunjucks = require('nunjucks');
+var isServer = typeof window === 'undefined';
+var serverOnlyLayoutFinderPath = './server/layoutFinder';
+var localExports = {};
+
+if (!isServer) {
+    nunjucks = window.nunjucks;
+}
+nunjucks = nunjucks.configure('app/templates', {
+    watch: false
+});
+
+localExports.nunjucks = nunjucks;
+
+localExports.getTemplate = require('./shared/templateFinder')(nunjucks).getTemplate;
+
+if (isServer) {
+    localExports.getLayout = require(serverOnlyLayoutFinderPath)(nunjucks).getLayout;
+}
+else {
+    localExports.getLayout = function() {
+        throw new Error('getLayout is only available on the server.');
+    };
+}
+
+localExports.registerExtensions = function(extensions) {
+    extensions = extensions(nunjucks);
+    for (var extension in extensions) {
+        nunjucks.addExtension(extension, new extensions[extension]);
+    }
+};
+localExports.registerExtensions(require('./shared/extensions'));
+
+localExports.registerHelpers = function(helpers) {
+    helpers = helpers(nunjucks);
+    for (var helper in helpers) {
+        nunjucks.addFilter(helper, helpers[helper]);
+    }
+};
+localExports.registerHelpers(require('./shared/helpers'));
 
 module.exports = function() {
-    var localExports = {
-        init: init
-    };
-
-    function init() {
-        nunjucks = nunjucks || (function(nunjucks) {
-            if (typeof window !== 'undefined') {
-                nunjucks = window.nunjucks;
-            }
-            return nunjucks.configure('app/templates', {
-                watch: false
-            });
-        })(require('nunjucks'));
-
-        localExports.nunjucks = nunjucks;
-        localExports.getTemplate = require('./shared/templateFinder')(nunjucks).getTemplate;
-        if (typeof window === 'undefined') {
-            var serverOnlyLayoutFinderPath = './server/layoutFinder';
-
-            // server only, "hide" it from r.js compiler by having require statement with variable
-            localExports.getLayout = require(serverOnlyLayoutFinderPath)(nunjucks).getLayout;
-        }
-        else {
-            localExports.getLayout = function() {
-                throw new Error('getLayout is only available on the server.');
-            };
-        }
-        localExports.registerExtensions = function(extensions) {
-            extensions = extensions(nunjucks);
-            for (var extension in extensions) {
-                nunjucks.addExtension(extension, new extensions[extension]);
-            }
-        };
-        localExports.registerExtensions(require('./shared/extensions'));
-        localExports.registerHelpers = function(helpers) {
-            helpers = helpers(nunjucks);
-            for (var helper in helpers) {
-                nunjucks.addFilter(helper, helpers[helper]);
-            }
-        };
-        localExports.registerHelpers(require('./shared/helpers'));
-    }
-
     return localExports;
-}
+};
